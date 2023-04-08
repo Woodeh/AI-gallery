80f3857754523671f10379a9f093a4c38746916b
Arkadiy <woodythps@gmail.com>
2023-04-09 00:37:59 +0300

    update

--- src/script.js
+++ src/script.js
@@ -86,3 +86,47 @@ function toggleSubmenu() {
 }
 // END SideNavJS
 
+
+
+// Open the modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Display the selected slide
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Move to the previous slide
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Move to the selected slide
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Display the slides
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
